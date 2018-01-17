544f1f401c50704ffab60b8c03c8748f786bd5ef
Thomas Wilson <twilson.spqr@gmail.com>
2018-01-17 00:12:03 -0500

    reacted stuff

--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -4,51 +4,130 @@ import TextField from '../components/TextField'
 class FormContainer extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      errors: {},
+      firstName: '',
+      lastName: '',
+      streetAddress: '',
+      cityName: '',
+      stateName: '',
+      zipCode: '',
+      phoneNumber: '',
+      eMail: '',
+      fields: [
+        ["firstName", "First Name"],
+        ["lastName", "Last Name"],
+        ["streetAddress", "Street Address"],
+        ["cityName", "City Name"],
+        ["stateName", "State Name"],
+        ["zipCode", "Zip Code"],
+        ["phoneNumber", "Phone Number"],
+        ["eMail", "eMail"]
+      ]
+
+    }
+    this.handleClearForm = this.handleClearForm.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  handleClearForm(event) {
+    event.preventDefault();
+    this.setState({
+      errors: {},
+      firstName: '',
+      lastName: '',
+      streetAddress: '',
+      cityName: '',
+      stateName: '',
+      zipCode: '',
+      phoneNumber: '',
+      eMail: ''
+    })
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+    let formPayload = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      streetAddress: this.state.streetAddress,
+      cityName: this.state.cityName,
+      stateName: this.state.stateName,
+      zipCode: this.state.zipCode,
+      phoneNumber: this.state.phoneNumber,
+      eMail: this.state.eMail
+    };
+      console.log(this.state)
+      this.handleClearForm(event);
+  }
+
+  handleChange(event) {
+    let name = event.target.name
+    let value = event.target.value
+    this.setState({[name]: value})
+  }
+
+
   render() {
+    let listOfFields;
+
     return (
-      <form className="callout" id="shipping-address-form">
-        <h1>Shipping Address</h1>
-        <div>
-          <label htmlFor="firstName">First Name:</label>
-          <input type="text" id="first-name" name="firstName" />
+      <form className="callout" onSubmit={this.handleFormSubmit}>
+      <h1>Shipping Address</h1>
+        <TextField
+          content={this.state.firstName}
+          label='First Name'
+          name='firstName'
+          handlerFunction={this.handleChange}
+        />
+        <TextField
+          content={this.state.lastName}
+          label='Last Name'
+          name='lastName'
+          handlerFunction={this.handleChange}
+        />
+        <TextField
+          content={this.state.streetAddress}
+          label='Street Address'
+          name='streetAddress'
+          handlerFunction={this.handleChange}
+        />
+        <TextField
+          content={this.state.cityName}
+          label='City'
+          name='cityName'
+          handlerFunction={this.handleChange}
+        />
+        <TextField
+          content={this.state.stateName}
+          label='State'
+          name='stateName'
+          handlerFunction={this.handleChange}
+        />
+        <TextField
+          content={this.state.zipCode}
+          label='Zip Code'
+          name='zipCode'
+          handlerFunction={this.handleChange}
+        />
+        <TextField
+          content={this.state.phoneNumber}
+          label='Phone Number'
+          name='phoneNumber'
+          handlerFunction={this.handleChange}
+        />
+        <TextField
+          content={this.state.eMail}
+          label='eMail'
+          name='eMail'
+          handlerFunction={this.handleChange}
+        />
+
+        <div className="button-group">
+          <button className="button" onClick={this.handleClearForm}>Clear</button>
+          <input className="button" type="submit" value="Submit" />
         </div>
-
-        <div>
-          <label htmlFor="lastName">Last Name:</label>
-          <input type="text" id="last-name" name="lastName" />
-        </div>
-
-        <label htmlFor="address">Address:</label>
-        <input type="text" id="address" name="address" />
-
-        <div>
-          <label htmlFor="city">City:</label>
-          <input type="text" id="city" name="city" />
-        </div>
-
-        <div>
-          <label htmlFor="state">State:</label>
-          <input type="text" id="state" name="state" />
-        </div>
-
-        <div>
-          <label htmlFor="zipCode">Zip Code:</label>
-          <input type="text" id="zip-code" name="zipCode" />
-        </div>
-
-        <div>
-          <label htmlFor="phoneNumber">Phone Number:</label>
-          <input type="text" id="phone-number" name="phoneNumber" />
-        </div>
-
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="text" id="email" name="email" />
-        </div>
-
-        <input type="submit" className="button" value="Submit "/>
       </form>
     )
   }
